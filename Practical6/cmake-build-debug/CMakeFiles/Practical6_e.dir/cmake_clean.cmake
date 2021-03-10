69736c78811547cc234e00c35015c07934b13d02
GOCJM <cjm00080@students.stir.ac.uk>
2021-03-10 15:52:57 +0000

    Practical7 is now finished.

---
+++ Practical6/cmake-build-debug/CMakeFiles/Practical6_e.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Practical6_e.dir/exercise1.c.o"
+  "Practical6_e"
+  "Practical6_e.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Practical6_e.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Practical7/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#define N 10
+
+int max_min(int *array, int array_size, int *large, int *small) {
+    int small_current = *array, large_current = *array;
+    int *pointer = array;
+    for (int i = 0; i < array_size; ++i,pointer++) {
+        if (*pointer < small_current){
+            small_current = *pointer;
+        }
+        if (*pointer > large_current){
+            large_current = *pointer;
+        }
+    }
+    *large = large_current;
+    *small = small_current;
+}
+
+int main() {
+    int b[N], big, small;
+    printf("Enter %d numbers:\t",N);
+    for (int i = 0; i < N; ++i) {
+        scanf(" %d",&b[i]);
+    }
+
+    max_min(b, N, &big, &small);
+
+    printf("\nLargest:\t%d",big);
+    printf("\nSmallest:\t%d",small);
+
+    return 0;
+}
